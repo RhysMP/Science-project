74548bad8d40ca2e57f3959f9634e86b7f20362c
rhysz <41348612+rhysz@users.noreply.github.com>
2018-09-25 21:12:32 -0700

    Update Python Cracking Script.py

--- Python Cracking Script.py
+++ Python Cracking Script.py
@@ -7,7 +7,7 @@ import random
 then = time.time() 
 
 def guess_password(real):
-    chars = string.digits
+    chars = string.ascii_letters + string.digits + string.punctuation 
     attempts = 0
     for password_length in range(1, 9):
         for guess in itertools.product(chars, repeat=password_length):
@@ -17,7 +17,7 @@ def guess_password(real):
                 return 'password is {}. found in {} attempt(s).'.format(guess, attempts)
             print(guess, attempts)
 
-print(guess_password('12011998'))
+print(guess_password('PASSWORD HERE'))
 now = time.time() 
 
 print("Taking: ", now-then, " seconds")
